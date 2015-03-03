46b43236c08613959f9c684acd8a568c87c9f31a
Craig Spaeth <craigspaeth@Craigs-MacBook-Pro.local>
2015-03-03 01:28:49 -0500

    wired up Backbone UI

--- sample/scripts/app.js
+++ sample/scripts/app.js
@@ -1,9 +1,108 @@
-var tmpl = [
-  '<div>',
-    '<h1><%= name %></h1>',
-  '</div>'
-].join('')
+// 
+// Top-level collections of favorites and the list of kittens
+// 
+var kittens = new Backbone.Collection();
+var favorites = new Backbone.Collection();
 
+// 
+// The main view that renders the list of kittens
+// 
+var ListView = Backbone.View.extend({
+
+  template: _.template(
+    '<ul>' + 
+      '<% favorites.each(function(kitten) { %>' +
+        '<li data-id="<%= kitten.get("id") %>">' + 
+          '<img src="images/<%= kitten.get("href") %>.jpg">' +
+          '<%= kitten.get("name") %>' +
+        '</li>' +
+      '<% }); %>' +
+    '</ul>'
+  ),
+
+  initialize: function(options) {
+    this.favorites = options.favorites;
+    this.collection.on('add', _.bind(this.render, this));
+    this.favorites.on('remove', _.bind(this.onRemoveFavorite, this))
+    this.nextPage();
+  },
+
+  onRemoveFavorite: function(kitten) {
+    this.$('[data-id="' + kitten.get('id') + '"]').removeClass('favorited');
+  },
+
+  render: function() {
+    this.$('#favorites-list').html(
+      this.template({ favorites: this.collection })
+    );
+  },
+
+  events: {
+    'click #next-page': 'nextPage',
+    'click li': 'favoriteKitten'
+  },
+
+  nextPage: function() {
+    this.collection.add(_.times(30, function() {
+      return _.sample(fixtures());
+    }));
+  },
+
+  favoriteKitten: function(event) {
+    var id = Number($(event.currentTarget).attr('data-id'));
+    var kitten = this.collection.get(id);
+    this.favorites.add(kitten);
+    $(event.currentTarget).addClass('favorited');
+  }
+});
+
+// 
+// The favorites tray that sits at the bottom
+// 
+var TrayView = Backbone.View.extend({
+
+  template: _.template(
+    '<ul>' + 
+      '<% favorites.each(function(kitten) { %>' +
+        '<li data-id="<%= kitten.get("id") %>">' + 
+          '<img src="images/<%= kitten.get("href") %>.jpg">' +
+        '</li>' +
+      '<% }); %>' +
+    '</ul>'
+  ),
+
+  initialize: function() {
+    this.collection.on('add remove', _.bind(this.render, this));
+  },
+
+  render: function() {
+    this.$el.html(
+      this.template({ favorites: this.collection })
+    );
+  },
+
+  events: {
+    'click li': 'remove'
+  },
+
+  remove: function(event) {
+    var id = Number($(event.currentTarget).attr('data-id'));
+    var kitten = this.collection.get(id);
+    this.collection.remove(kitten);
+  }
+});
+
+// 
+// Initialize function that sets everything up
+// 
 $(function() {
-  $('#favorites-list').html(_.template(tmpl)({ name: 'foo' }));
+  new TrayView({
+    el: $('#favorites-tray'),
+    collection: favorites
+  });
+  new ListView({
+    el: $('#favorites'),
+    collection: kittens,
+    favorites: favorites
+  });
 });
---
+++ sample/scripts/fixtures.js
@@ -0,0 +1,25 @@
+var fixtures = function() {
+  return [
+    { name: 'Fluffers', href: 0, id: _.uniqueId() },
+    { name: 'Piper', href: 1, id: _.uniqueId() },
+    { name: 'Pimples', href: 2, id: _.uniqueId() },
+    { name: 'Cutie', href: 3, id: _.uniqueId() },
+    { name: 'Boxer', href: 4, id: _.uniqueId() },
+    { name: 'Nibbles', href: 5, id: _.uniqueId() },
+    { name: 'Kit', href: 6, id: _.uniqueId() },
+    { name: 'Kat', href: 7, id: _.uniqueId() },
+    { name: 'Ratatat', href: 8, id: _.uniqueId() },
+    { name: 'Bashful', href: 9, id: _.uniqueId() },
+    { name: 'Donner', href: 10, id: _.uniqueId() },
+    { name: 'Blitzen', href: 11, id: _.uniqueId() },
+    { name: 'Sir Fluff', href: 12, id: _.uniqueId() },
+    { name: 'Mangey', href: 13, id: _.uniqueId() },
+    { name: 'Tangy', href: 14, id: _.uniqueId() },
+    { name: 'Spot', href: 15, id: _.uniqueId() },
+    { name: 'Bob', href: 16, id: _.uniqueId() },
+    { name: 'Joe', href: 17, id: _.uniqueId() },
+    { name: 'Sarah', href: 18, id: _.uniqueId() },
+    { name: 'Rachel', href: 19, id: _.uniqueId() },
+    { name: 'Dixon', href: 20, id: _.uniqueId() }
+  ];
+}
